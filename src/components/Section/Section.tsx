cedd037a30e61bbbc148613ba0f10b61998b3417
Jan Bárta <jbarta.edu@gmail.com>
2023-01-03 14:47:42 +0100

    feat(homepage): Added generic Section component for pages

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,17 @@
+import React, { PropsWithChildren } from "react";
+import styles from "./styles.module.css";
+
+interface SectionProps extends PropsWithChildren  {
+    heading?: string;
+}
+
+export default function Section ({ heading, children }: SectionProps) {
+    return (
+        <section className={styles.section}>
+            <div className="container">
+                {heading && <h2>{heading}</h2>}
+                {children}
+            </div>
+        </section>
+    );
+}
