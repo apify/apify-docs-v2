2bc9ff7ca51bf6023c44cd5d3a52bfb17c77ac10
Jindřich Bär <jindrichbar@gmail.com>
2023-01-13 14:53:08 +0100

    feat: autogenerate baseUrl/changelog from the root repo changelog (#23)

--- apify-docs-theme/src/theme.js
+++ apify-docs-theme/src/theme.js
@@ -1,6 +1,22 @@
+const path = require('path');
+const fs = require('fs');
+
+function findChangelogPath() {
+    let changelogPath = __dirname;
+    while (changelogPath !== path.join(changelogPath, '..')) {
+        const filePath = path.join(changelogPath, 'CHANGELOG.md');
+        if (fs.existsSync(filePath)) return filePath;
+        changelogPath = path.join(changelogPath, '..');
+    }
+    const filePath = path.join(changelogPath, 'CHANGELOG.md');
+    if (fs.existsSync(filePath)) return filePath;
+
+    throw new Error('Could not find CHANGELOG.md in any parent directory');
+}
+
 function theme(
-    context, 
-    options
+    context,
+    options,
 ) {
     return {
         name: '@apify/docs-theme',
@@ -10,11 +26,30 @@ function theme(
         getTypeScriptThemePath() {
             return '../src/theme';
         },
-        contentLoaded({ actions }) {
+        async contentLoaded({ actions }) {
             const { setGlobalData } = actions;
             setGlobalData({
                 options,
             });
+
+            try {
+                const changelog = fs.readFileSync(findChangelogPath(), 'utf8');
+                const dataPath = await actions.createData(
+                    'changelog.json',
+                    JSON.stringify(changelog),
+                );
+
+                actions.addRoute({
+                    path: path.join(context.baseUrl, 'changelog'),
+                    component: require.resolve('./pages/ChangelogPage.jsx'),
+                    exact: true,
+                    modules: {
+                        changelog: dataPath,
+                    },
+                });
+            } catch (e) {
+                console.warn(`Changelog page could not be initialized: ${e.message}`);
+            }
         },
         getClientModules() {
             return [
@@ -26,7 +61,7 @@ function theme(
                 module: {
                     rules: [
                         {
-                            test: /(@apify\/|apify-)docs-theme\/src\/theme\/.*?\.jsx?$/,
+                            test: /(@apify\/|apify-)docs-theme\/src\/(theme|pages)\/.*?\.jsx?$/,
                             use: {
                                 loader: 'babel-loader',
                             },
